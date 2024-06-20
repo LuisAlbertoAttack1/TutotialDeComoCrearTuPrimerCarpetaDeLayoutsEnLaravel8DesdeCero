8f57dbfefc619703fb7790b4416a66c2bee4a987
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2024-06-19 23:49:31 -0600

    NuevoProyectoAvanceDeVideo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.plantilla')
+
+@section('TituloPagina','CrudLaravelPractica')
+
+@section('contenido')
+    <h1>Funciona Todo</h1>
+    <div class="row">
+        <div class="col">
+            <!--Seccion 1-->
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            <!--Seccion 2-->
+        </div>
+    </div>
+@endsection
